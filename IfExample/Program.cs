9f4fae8d9feb95508022aaded8ce08f1ff5c01cf
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-17 17:45:35 +0100

    If, If-else, Else-if, Nested-if

--- IfExample/Program.cs
+++ IfExample/Program.cs
@@ -2,11 +2,46 @@
 
 namespace IfExample
 {
-	internal class Program
+	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			// Declare variable to store marks of the student
+			int marks = 70;
+			char gradeLetter;
+
+			// >=85 O
+			// >=60 && <85 A
+			// >=50 && <50 B
+			// >=35 && <50 C
+			// else F
+
+			// Show congratulations if the student has passed
+			if (marks >= 35)
+			{
+				if (marks >= 85)
+				{
+					gradeLetter = 'O';
+				}
+				else if (marks >= 60 && marks < 85)
+				{
+					gradeLetter = 'A';
+				}
+				else if (marks >= 50 && marks < 60)
+				{
+					gradeLetter = 'B';
+				}
+				else
+				{
+					gradeLetter = 'C';
+				}
+			} else
+			{
+				gradeLetter = 'F';
+			}
+
+			System.Console.WriteLine("Your grade is " + gradeLetter);
+			System.Console.ReadKey();
 		}
 	}
 }
